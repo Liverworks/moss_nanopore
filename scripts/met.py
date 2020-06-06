19917a2707089ffe70fa2baca6afa32f4c2cd3fb
Liverworks <zverochek@yahoo.ca>
2020-06-06 15:46:45 +0300

    minor corrections

--- scripts/met.py
+++ scripts/met.py
@@ -1,11 +1,9 @@
 from nanocompore.SampCompDB import SampCompDB, jhelp
 
-db = SampCompDB (
-    db_fn = "/home/andrey/anna/methilation3/simulated_SampComp.db",
-    fasta_fn = "/home/andrey/minimap2/Ppatens_318_v3.fa")
-
-# Print general metadata information
-print (db)
-
-# Prit list of references containing valid data
-print (db.ref_id_list)
+# Recommended NanosampComp script
+jhelp(SampCompDB)
+db = SampCompDB(db_fn = "out_SampComp.db", fasta_fn = "/media/ilia/anna/transcriptome_dupl.fa", log_level="warning")
+db.save_report(output_fn="report.tsv")
+db.save_shift_stats (output_fn="shift.tsv")
+db = SampCompDB(db_fn = "out_SampComp.db", fasta_fn = "/media/ilia/anna/transcriptome_dupl.fa", bed_fn="/home/andrey/anna/annot_genes.bed")
+db.save_to_bed (output_fn="sig_positions.bed", pvalue_field="KS_intensity_pvalue")
